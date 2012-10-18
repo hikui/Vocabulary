c6f1149880f58a693b53ef7e7c97ed2336a65351
hikui <hikuimiao@gmail.com>
2012-10-18 23:31:47 +0800

    change dir

---
+++ Vocabulary/WordList.m
@@ -0,0 +1,20 @@
+//
+//  WordList.m
+//  Vocabulary
+//
+//  Created by 缪和光 on 12-10-18.
+//  Copyright (c) 2012年 缪和光. All rights reserved.
+//
+
+#import "WordList.h"
+#import "Word.h"
+
+
+@implementation WordList
+
+@dynamic addTime;
+@dynamic lastReviewTime;
+@dynamic reviewCount;
+@dynamic words;
+
+@end
