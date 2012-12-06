7a2288fcc467e10fdea597649c0152add051b054
hikui <hikuimiao@gmail.com>
2012-12-06 16:46:04 +0800

    所有文件加入GPL授权

--- Vocabulary/CoreDataHelper.h
+++ Vocabulary/CoreDataHelper.h
@@ -1,3 +1,20 @@
+
+/*
+ *  This file is part of 记词助手.
+ *
+ *	记词助手 is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License Version 2 as 
+ *  published by the Free Software Foundation.
+ *
+ *	记词助手 is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with 记词助手.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //
 //  CoreDataHelper.h
 //  Vocabulary
