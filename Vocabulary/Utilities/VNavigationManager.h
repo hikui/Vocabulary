8c3bafcfc8c60e4b753845c673e77354079ad537
缪和光 <hikuimiao@gmail.com>
2014-12-23 18:07:53 +0800

    imagepicker

--- Vocabulary/Utilities/VNavigationManager.h
+++ Vocabulary/Utilities/VNavigationManager.h
@@ -8,8 +8,8 @@
 
 #import <Foundation/Foundation.h>
 
-extern NSString * const VNavigationConfigClassNameKey;
-extern NSString * const VNavigationConfigXibNameKey;
+extern NSString* const VNavigationConfigClassNameKey;
+extern NSString* const VNavigationConfigXibNameKey;
 
 typedef NS_ENUM(NSInteger, VNavigationActionType) {
     VNavigationActionTypePush,
@@ -19,10 +19,10 @@ typedef NS_ENUM(NSInteger, VNavigationActionType) {
     VNavigationActionTypeResetRoot
 };
 
-@interface VNavigationActionCommand : NSObject<NSCopying>
+@interface VNavigationActionCommand : NSObject <NSCopying>
 
 @property (nonatomic, assign) VNavigationActionType actionType;
-@property (nonatomic, copy) NSURL *targetURL;
+@property (nonatomic, copy) NSURL* targetURL;
 
 /**
  如果为true，将pop最顶层的view controller再push新的view controller
@@ -34,22 +34,22 @@ typedef NS_ENUM(NSInteger, VNavigationActionType) {
  用于初始化view controller的参数，key为目标view controller的properties的名字
  value为需要注入的值。将会通过KVC注入。
  */
-@property (nonatomic, copy) NSDictionary *params;
+@property (nonatomic, copy) NSDictionary* params;
 
 @property (nonatomic, assign) BOOL animate;
 
 @end
 
+@interface VNavigationManager : NSObject <UINavigationControllerDelegate>
 
-@interface VNavigationManager : NSObject<UINavigationControllerDelegate>
-
-@property (nonatomic, weak) UINavigationController *navigationController;
+@property (nonatomic, weak) UINavigationController* navigationController;
 
 /**
  当URL匹配失败时，交给这个block处理
  command: 匹配失败的command
+ block的实现应根据command中的URL，返回一个UIViewController。如果返回nil，则该command被忽略。
  */
-@property (nonatomic, copy) UIViewController* (^onMatchFailureBlock)(VNavigationActionCommand *command);
+@property (nonatomic, copy) UIViewController* (^onMatchFailureBlock)(VNavigationActionCommand* command);
 
 + (instancetype)sharedInstance;
 
@@ -60,13 +60,13 @@ typedef NS_ENUM(NSInteger, VNavigationActionType) {
     key为NSURL, value为ViewController的class name和xib name组成的子dict
  */
 - (void)configRoute:(NSDictionary* (^)(void))routeConfigBlock;
-- (void)executeCommand:(VNavigationActionCommand *)command;
+- (void)executeCommand:(VNavigationActionCommand*)command;
 
 /* 简便方法 */
 - (void)commonPopAnimated:(BOOL)animate;
-- (void)commonPopToURL:(NSURL *)url animate:(BOOL)animate;
-- (void)commonPushURL:(NSURL *)url params:(NSDictionary *)params animate:(BOOL)animate;
-- (void)commonResetRootURL:(NSURL *)url params:(NSDictionary *)params;
-- (void)commonPresentModalURL:(NSURL *)url params:(NSDictionary *)params animate:(BOOL)animate;
+- (void)commonPopToURL:(NSURL*)url animate:(BOOL)animate;
+- (void)commonPushURL:(NSURL*)url params:(NSDictionary*)params animate:(BOOL)animate;
+- (void)commonResetRootURL:(NSURL*)url params:(NSDictionary*)params;
+- (void)commonPresentModalURL:(NSURL*)url params:(NSDictionary*)params animate:(BOOL)animate;
 - (void)commonDismissModalAnimated:(BOOL)animate;
 @end
--- Vocabulary/Utilities/VNavigationManager.m
+++ Vocabulary/Utilities/VNavigationManager.m
@@ -8,13 +8,16 @@
 
 #import "VNavigationManager.h"
 
-NSString * const VNavigationConfigClassNameKey = @"VNavigationConfigClassNameKey";
-NSString * const VNavigationConfigXibNameKey = @"VNavigationConfigXibNameKey";
+NSString* const VNavigationConfigClassNameKey =
+    @"VNavigationConfigClassNameKey";
+NSString* const VNavigationConfigXibNameKey = @"VNavigationConfigXibNameKey";
 
 @implementation VNavigationActionCommand
 
-- (id)copyWithZone:(NSZone *)zone {
-    VNavigationActionCommand *copyCommand = [[VNavigationActionCommand allocWithZone:zone]init];
+- (id)copyWithZone:(NSZone*)zone
+{
+    VNavigationActionCommand* copyCommand =
+        [[VNavigationActionCommand allocWithZone:zone] init];
     copyCommand.actionType = self.actionType;
     copyCommand.targetURL = [self.targetURL copy];
     copyCommand.animate = self.animate;
@@ -27,19 +30,19 @@ NSString * const VNavigationConfigXibNameKey = @"VNavigationConfigXibNameKey";
 
 @interface VNavigationManager ()
 
-@property (nonatomic, copy) NSDictionary *routes;
-@property (nonatomic, strong) NSMutableArray *commandQueue;
+@property (nonatomic, copy) NSDictionary* routes;
+@property (nonatomic, strong) NSMutableArray* commandQueue;
 
 @end
 
 @implementation VNavigationManager
 
-+ (instancetype)sharedInstance {
-    static VNavigationManager *sharedManager;
++ (instancetype)sharedInstance
+{
+    static VNavigationManager* sharedManager;
     static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedManager = [[VNavigationManager alloc]init];
-    });
+    dispatch_once(&onceToken,
+                  ^{ sharedManager = [[VNavigationManager alloc] init]; });
     return sharedManager;
 }
 
@@ -47,126 +50,153 @@ NSString * const VNavigationConfigXibNameKey = @"VNavigationConfigXibNameKey";
 {
     self = [super init];
     if (self) {
-        _commandQueue = [[NSMutableArray alloc]init];
-//        _isPerformingNavigationAction = NO;
+        _commandQueue = [[NSMutableArray alloc] init];
     }
     return self;
 }
 
-- (void)configRoute:(NSDictionary* (^)(void))routeConfigBlock {
+- (void)configRoute:(NSDictionary* (^)(void))routeConfigBlock
+{
     if (!routeConfigBlock) {
         return;
     }
-    
-    NSDictionary *routes = routeConfigBlock();
-    NSMutableDictionary *validRoutes = [[NSMutableDictionary alloc]initWithCapacity:routes.count];
-    [routes enumerateKeysAndObjectsUsingBlock:^(NSURL *key, NSDictionary *obj, BOOL *stop) {
-        if (![key isKindOfClass:[NSURL class]] || ![obj isKindOfClass:[NSDictionary class]]) {
-            // 确保类型正确
-            return;
-        }
-        [validRoutes setObject:obj forKey:obj];
+
+    NSDictionary* routes = routeConfigBlock();
+    NSMutableDictionary* validRoutes =
+        [[NSMutableDictionary alloc] initWithCapacity:routes.count];
+    [routes enumerateKeysAndObjectsUsingBlock:^(NSURL* key,
+                                                NSDictionary* obj,
+                                                BOOL* stop) {
+      if (![key isKindOfClass:[NSURL class]] ||
+          ![obj isKindOfClass:[NSDictionary class]]) {
+        // 确保类型正确
+        return;
+      }
+      [validRoutes setObject:obj forKey:obj];
     }];
-    
+
     self.routes = routes;
-    
 }
 
-- (void)commonPopAnimated:(BOOL)animate {
+- (void)executeCommand:(VNavigationActionCommand*)command
+{
+    [self.commandQueue addObject:command];
+
+    if (self.commandQueue.count == 1) {
+        [self _executeNextCommand];
+    }
+}
+
+- (void)commonPopAnimated:(BOOL)animate
+{
     [self commonPopToURL:nil animate:animate];
 }
-- (void)commonPopToURL:(NSURL *)url animate:(BOOL)animate {
-    VNavigationActionCommand *command = [VNavigationActionCommand new];
+
+- (void)commonPopToURL:(NSURL*)url animate:(BOOL)animate
+{
+    VNavigationActionCommand* command = [VNavigationActionCommand new];
     command.actionType = VNavigationActionTypePop;
     command.animate = animate;
     command.targetURL = url;
     [self executeCommand:command];
 }
-- (void)commonPushURL:(NSURL *)url params:(NSDictionary *)params animate:(BOOL)animate {
-    VNavigationActionCommand *command = [VNavigationActionCommand new];
+
+- (void)commonPushURL:(NSURL*)url
+               params:(NSDictionary*)params
+              animate:(BOOL)animate
+{
+    VNavigationActionCommand* command = [VNavigationActionCommand new];
     command.actionType = VNavigationActionTypePush;
     command.animate = animate;
     command.targetURL = url;
     command.params = params;
     [self executeCommand:command];
 }
-- (void)commonResetRootURL:(NSURL *)url params:(NSDictionary *)params {
-    VNavigationActionCommand *command = [VNavigationActionCommand new];
+
+- (void)commonResetRootURL:(NSURL*)url params:(NSDictionary*)params
+{
+    VNavigationActionCommand* command = [VNavigationActionCommand new];
     command.actionType = VNavigationActionTypeResetRoot;
     command.targetURL = url;
     command.params = params;
     [self executeCommand:command];
 }
-- (void)commonPresentModalURL:(NSURL *)url params:(NSDictionary *)params animate:(BOOL)animate {
-    VNavigationActionCommand *command = [VNavigationActionCommand new];
+
+- (void)commonPresentModalURL:(NSURL*)url
+                       params:(NSDictionary*)params
+                      animate:(BOOL)animate
+{
+    VNavigationActionCommand* command = [VNavigationActionCommand new];
     command.actionType = VNavigationActionTypePresentModal;
     command.targetURL = url;
     command.params = params;
     command.animate = animate;
     [self executeCommand:command];
 }
-- (void)commonDismissModalAnimated:(BOOL)animate {
-    VNavigationActionCommand *command = [VNavigationActionCommand new];
+
+- (void)commonDismissModalAnimated:(BOOL)animate
+{
+    VNavigationActionCommand* command = [VNavigationActionCommand new];
     command.actionType = VNavigationActionTypeDismissModal;
     command.animate = animate;
     [self executeCommand:command];
 }
 
-- (void)_executeNextCommand {
-    VNavigationActionCommand *nextCommand = [self.commandQueue firstObject];
+- (void)_executeNextCommand
+{
+    VNavigationActionCommand* nextCommand = [self.commandQueue firstObject];
     switch (nextCommand.actionType) {
-        case VNavigationActionTypePush:
-            [self _doPush:nextCommand];
-            break;
-        case VNavigationActionTypePop:
-            [self _doPop:nextCommand];
-            break;
-        case VNavigationActionTypePresentModal:
-            [self _doPresentModal:nextCommand];
-            break;
-        case VNavigationActionTypeResetRoot:
-            [self _doResetRoot:nextCommand];
-            break;
-        case VNavigationActionTypeDismissModal:
-            [self _doDismissModal:nextCommand];
-            break;
-        default:
-            break;
+    case VNavigationActionTypePush:
+        [self _doPush:nextCommand];
+        break;
+    case VNavigationActionTypePop:
+        [self _doPop:nextCommand];
+        break;
+    case VNavigationActionTypePresentModal:
+        [self _doPresentModal:nextCommand];
+        break;
+    case VNavigationActionTypeResetRoot:
+        [self _doResetRoot:nextCommand];
+        break;
+    case VNavigationActionTypeDismissModal:
+        [self _doDismissModal:nextCommand];
+        break;
+    default:
+        break;
     }
 }
 
-- (void)executeCommand:(VNavigationActionCommand *)command {
-    [self.commandQueue addObject:command];
-    
-    if (self.commandQueue.count == 1) {
-        [self _executeNextCommand];
-    }
-}
-
-- (void)_doPush:(VNavigationActionCommand *)command {
-    UIViewController *controller = [self _assembleViewControllerWithCommand:command];
+- (void)_doPush:(VNavigationActionCommand*)command
+{
+    UIViewController* controller =
+        [self _assembleViewControllerWithCommand:command];
     if (controller == nil) {
         [self _dequeueAndJudgeNextStep];
         return;
     }
     if (command.popTopBeforePush) {
-        NSMutableArray *viewControllers = [NSMutableArray arrayWithArray:self.navigationController.viewControllers];
+        NSMutableArray* viewControllers = [NSMutableArray
+            arrayWithArray:self.navigationController.viewControllers];
         if (viewControllers.count > 0) {
             [viewControllers removeLastObject];
         }
         [viewControllers addObject:controller];
-        [self.navigationController setViewControllers:viewControllers animated:command.animate];
-    }else{
-        [self.navigationController pushViewController:controller animated:command.animate];
+        [self.navigationController setViewControllers:viewControllers
+                                             animated:command.animate];
+    }
+    else {
+        [self.navigationController pushViewController:controller
+                                             animated:command.animate];
     }
 }
 
-- (void)_doPop:(VNavigationActionCommand *)command {
+- (void)_doPop:(VNavigationActionCommand*)command
+{
     // 如果command有targetURL，则尝试执行popToViewController
     // 如果在当前栈里找不到targetURL所属的类型，则转发到_doPush，并将popTopBeforePush设为YES
     if (command.targetURL) {
-        NSDictionary *configForURL = self.routes[command.targetURL];
-        NSString *classString = configForURL[VNavigationConfigClassNameKey];
+        NSDictionary* configForURL = self.routes[command.targetURL];
+        NSString* classString = configForURL[VNavigationConfigClassNameKey];
         Class controllerClass = nil;
         if (classString) {
             controllerClass = NSClassFromString(classString);
@@ -174,136 +204,170 @@ NSString * const VNavigationConfigXibNameKey = @"VNavigationConfigXibNameKey";
         if (!controllerClass) {
             // 未注册的URL，回调查询
             if (self.onMatchFailureBlock) {
-                UIViewController *defaultController = self.onMatchFailureBlock(command);
+                UIViewController* defaultController = self.onMatchFailureBlock(command);
                 if (defaultController) {
-                    controllerClass = [defaultController class]; // 这里需要找到class，因为controller stacks中不会存在这个实例，只会存在该实例所属的class的另外一个实例
-                }else{
+                    controllerClass =
+                        [defaultController class]; // 这里需要找到class，因为controller
+                    // stacks中不会存在这个实例，只会存在该实例所属的class的另外一个实例
+                }
+                else {
                     // 实在找不到，直接删除当前的command
                     [self _dequeueAndJudgeNextStep];
                     return;
                 }
             }
         }
-        NSArray *controllersInStack = self.navigationController.viewControllers;
-        UIViewController *targetViewController = nil;
+        NSArray* controllersInStack = self.navigationController.viewControllers;
+        UIViewController* targetViewController = nil;
         for (NSUInteger i = controllersInStack.count - 1; i > 0; i--) {
             // 从栈顶往下搜索
-            UIViewController *aViewController = controllersInStack[i];
-            // 注意这里要使用isMemberOfClass，避免子类view controller和父类view controller混淆
+            UIViewController* aViewController = controllersInStack[i];
+            // 注意这里要使用isMemberOfClass，避免子类view controller和父类view
+            // controller混淆
             if ([aViewController isMemberOfClass:controllerClass]) {
                 targetViewController = aViewController;
                 break;
             }
         }
         if (targetViewController) {
-            [self.navigationController popToViewController:targetViewController animated:command.animate];
-        }else{
+            [self.navigationController popToViewController:targetViewController
+                                                  animated:command.animate];
+        }
+        else {
             // 在栈中找不到需要的view controller
             // 转发command到push
             command.actionType = VNavigationActionTypePush;
             command.popTopBeforePush = YES;
             [self _doPush:command];
         }
-    } else {
+    }
+    else {
         if (self.navigationController.viewControllers.count == 1) {
-            // 最后一个view controller，pop操作会无效化，不会进delegate方法，这里直接删除queue中的东西
+            // 最后一个view
+            // controller，pop操作会无效化，不会进delegate方法，这里直接删除queue中的东西
             [self _dequeueAndJudgeNextStep];
-        }else{
+        }
+        else {
             [self.navigationController popViewControllerAnimated:command.animate];
         }
-        
     }
-    
 }
 
-- (void)_doPresentModal:(VNavigationActionCommand *)command {
-    UIViewController *controller = [self _assembleViewControllerWithCommand:command];
+- (void)_doPresentModal:(VNavigationActionCommand*)command
+{
+    UIViewController* controller =
+        [self _assembleViewControllerWithCommand:command];
     if (controller == nil) {
         [self _dequeueAndJudgeNextStep];
         return;
     }
-    [self.navigationController presentViewController:controller animated:command.animate completion:^{
-        [self _dequeueAndJudgeNextStep];
-    }];
+    [self.navigationController
+        presentViewController:controller
+                     animated:command.animate
+                   completion:^{ [self _dequeueAndJudgeNextStep]; }];
 }
 
-- (void)_doDismissModal:(VNavigationActionCommand *)command {
-    [self.navigationController dismissViewControllerAnimated:command.animate completion:^{
-        [self _dequeueAndJudgeNextStep];
-    }];
+- (void)_doDismissModal:(VNavigationActionCommand*)command
+{
+    [self.navigationController
+        dismissViewControllerAnimated:command.animate
+                           completion:^{ [self _dequeueAndJudgeNextStep]; }];
 }
 
-- (void)_doResetRoot:(VNavigationActionCommand *)command {
-    UIViewController *controller = [self _assembleViewControllerWithCommand:command];
+- (void)_doResetRoot:(VNavigationActionCommand*)command
+{
+    UIViewController* controller =
+        [self _assembleViewControllerWithCommand:command];
     if (controller == nil) {
         [self _dequeueAndJudgeNextStep];
     }
-    [self.navigationController setViewControllers:@[controller] animated:command.animate];
+    [self.navigationController setViewControllers:@[ controller ]
+                                         animated:command.animate];
 }
 
-- (UIViewController *)_assembleViewControllerWithCommand:(VNavigationActionCommand *)command {
-    NSURL *url = command.targetURL;
-    NSDictionary *configForURL = self.routes[url];
-    UIViewController *controller = [self _viewControllerFromConfigValue:configForURL];
+- (UIViewController*)_assembleViewControllerWithCommand:
+                         (VNavigationActionCommand*)command
+{
+    NSURL* url = command.targetURL;
+    NSDictionary* configForURL = self.routes[url];
+    UIViewController* controller =
+        [self _viewControllerFromConfigValue:configForURL];
     if (!controller) {
         if (self.onMatchFailureBlock) {
             controller = self.onMatchFailureBlock(command);
         }
-    }else{
+    }
+    else {
         [self _injectParams:command.params toViewController:controller];
     }
     return controller;
 }
 
-- (UIViewController *)_viewControllerFromConfigValue:(NSDictionary *)configValue{
-    NSString *className = configValue[VNavigationConfigClassNameKey];
-    NSString *xibName = configValue[VNavigationConfigXibNameKey];
+- (UIViewController*)_viewControllerFromConfigValue:(NSDictionary*)configValue
+{
+    NSString* className = configValue[VNavigationConfigClassNameKey];
+    NSString* xibName = configValue[VNavigationConfigXibNameKey];
     // 如果找不到对应的class，则直接生成WebViewController
     Class controllerClass = NSClassFromString(className);
     if (!controllerClass) {
         return nil;
     }
-    
-    UIViewController *controller = nil;
+
+    UIViewController* controller = nil;
     if (xibName) {
-        if ([xibName isEqual:[NSNull null]]) { // 如果是NSNull，则使用initWithNibName初始化方法，但参数是nil
+        if ([xibName
+                isEqual:
+                    [NSNull null]]) { // 如果是NSNull，则使用initWithNibName初始化方法，但参数是nil
             xibName = nil;
         }
-        controller = [[controllerClass alloc]initWithNibName:xibName bundle:nil];
-    }else{
-        controller = [[controllerClass alloc]init];
+        controller = [[controllerClass alloc] initWithNibName:xibName bundle:nil];
     }
-    
+    else {
+        controller = [[controllerClass alloc] init];
+    }
+
     return controller;
 }
 
-- (void)_injectParams:(NSDictionary *)params toViewController:(UIViewController *)viewController {
-    [params enumerateKeysAndObjectsUsingBlock:^(NSString *key, id obj, BOOL *stop) {
-        if (![key isKindOfClass:[NSString class]]) {
+- (void)_injectParams:(NSDictionary*)params
+     toViewController:(UIViewController*)viewController
+{
+    [params
+        enumerateKeysAndObjectsUsingBlock:^(NSString* key, id obj, BOOL* stop) {
+          if (![key isKindOfClass:[NSString class]]) {
             return;
-        }
-        if (![viewController respondsToSelector:NSSelectorFromString(key)]) {
+          }
+          if (![viewController respondsToSelector:NSSelectorFromString(key)]) {
             return;
-        }
-        [viewController setValue:obj forKey:key];
-    }];
+          }
+          [viewController setValue:obj forKey:key];
+        }];
 }
 
-- (void)setNavigationController:(UINavigationController *)navigationController {
+- (void)setNavigationController:(UINavigationController*)navigationController
+{
     _navigationController = navigationController;
     navigationController.delegate = self;
+    //当navigationController变换时，清空当前的commandQueue
+    [self.commandQueue removeAllObjects];
 }
 
 #pragma mark - navigation controller delegate
-- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-
+- (void)navigationController:(UINavigationController*)navigationController
+      willShowViewController:(UIViewController*)viewController
+                    animated:(BOOL)animated
+{
 }
 
-- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
+- (void)navigationController:(UINavigationController*)navigationController
+       didShowViewController:(UIViewController*)viewController
+                    animated:(BOOL)animated
+{
     [self _dequeueAndJudgeNextStep];
 }
 
-- (void)_dequeueAndJudgeNextStep {
+- (void)_dequeueAndJudgeNextStep
+{
     if (self.commandQueue.count > 0) {
         [self.commandQueue removeObjectAtIndex:0];
     }
@@ -311,6 +375,5 @@ NSString * const VNavigationConfigXibNameKey = @"VNavigationConfigXibNameKey";
         [self _executeNextCommand];
     }
 }
-
 
 @end
