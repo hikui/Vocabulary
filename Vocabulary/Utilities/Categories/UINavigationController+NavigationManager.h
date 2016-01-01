24a4751ffdca67ff4bbe54f6883df7882ddc7b96
Heguang Miao <hikuimiao@gmail.com>
2016-01-01 11:11:29 +0800

    UINavigationController+NavigationManager

---
+++ Vocabulary/Utilities/Categories/UINavigationController+NavigationManager.h
@@ -0,0 +1,16 @@
+//
+//  UINavigationController+NavigationManager.h
+//  Vocabulary
+//
+//  Created by Heguang Miao on 1/01/2016.
+//  Copyright © 2016 缪和光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HKVNavigationManager.h"
+
+@interface UINavigationController (NavigationManager)
+
+- (HKVNavigationManager *)v_navigationManager;
+
+@end
